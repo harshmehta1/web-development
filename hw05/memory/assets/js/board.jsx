bdc0a5e14c44c816d026423e71c62fe2d2262a32
harshmehta1 <harshsmehta1@gmail.com>
2018-02-11 16:56:55 -0500

    updated code

--- hw05/memory/assets/js/board.jsx
+++ hw05/memory/assets/js/board.jsx
@@ -38,7 +38,6 @@ class Board extends React.Component {
 
 
   gotView(view){
-    console.log("New view", view);
     this.setState(view.game);
   }
 
@@ -47,7 +46,6 @@ class Board extends React.Component {
     if(!tile.flipped && !tile.matched){
       if(count <= 2 && this.state.paused == false) {
         count = count + 1;
-        console.log(count)
         this.channel.push("click", { id: tile.id })
           .receive("ok", this.gotView.bind(this));
           if(count == 2){
@@ -92,17 +90,6 @@ class Board extends React.Component {
         </div>
       </div>
     )
-    // return (
-    //   <div className="game">
-    //     <div className="grid-container">{tile_list}</div>
-    //     <div className="below">
-    //       <div className="below-content">
-    //           <Button id="reset" onClick={this.restartGame.bind(this)}>Restart</Button>
-    //           <p id="score">Score: {this.state.score}</p>
-    //       </div>
-    //     </div>
-    //   </div>
-    // )
   };
 
 }
@@ -136,143 +123,6 @@ function Tile(props) {
   } else {
     style = "tile"
   }
-  // return <Button className={style} val={card1.val} id={card1.id}>{name}</Button>
-
   return <Button className={style} val={card1.val} id={card1.id} onClick={() => props.clickTile(card1)}>{name}</Button>
-    // return <Button>xyz</Button>
 
 }
-
-
-
-
-
-// //handles the click event of tile
-//   clickTile(card) {
-//     //if the game is not paused or card has not matched or isnt flipped
-//     // do the following
-//     if(!this.state.paused){
-//       if(!card.matched){
-//         if(!card.flipped){
-//           //clone selected tiles array
-//           let tilesSelected = this.state.selectedTiles.slice();
-//
-//           //if not more than 2 tiles are selected, do the following
-//           if (tilesSelected.length<2){
-//             this.flipCard(card);
-//             let clickedTile = card;
-//             tilesSelected.push(clickedTile);
-//             let st;
-//             let clicks = this.state.clicks;
-//             clicks = clicks + 1;
-//
-//             if(tilesSelected.length==2){
-//                 st = _.extend(this.state, {
-//                   selectedTiles: tilesSelected,
-//                   paused: true,
-//                   clicks: clicks
-//                 });
-//                 setTimeout(() => {this.checkMatch()}, 1000);
-//               } else {
-//                 st = _.extend(this.state, {
-//                   selectedTiles: tilesSelected,
-//                   clicks: clicks
-//                 });
-//               }
-//
-//             this.setState(st);
-//         }
-//       }
-//     }
-//   }
-// }
-
-//flips the tile on click
-//   flipCard(card){
-//
-//     let xs = _.map(this.state.tiles, (tile) =>{
-//       if (tile.id == card.id) {
-//         return _.extend(tile, {flipped: !card.flipped});
-//       } else {
-//         return tile;
-//       }
-//     });
-//
-//     let st = _.extend(this.state, {
-//       tiles: xs
-//     });
-//
-//     this.setState(st);
-//
-//   }
-//
-//   //restarts the game by initializing new set of tiles
-//   restartGame(){
-//     this.setState(this.newTiles());
-//   }
-//
-// //checks if the two selected tiles have matched
-//   checkMatch(){
-//     //clone array
-//     let selectedTiles = this.state.selectedTiles.slice();
-//     let newScore = this.state.score;
-//     if(selectedTiles[0].val == selectedTiles[1].val){
-//       //matched - success
-//       let id1 = selectedTiles[0].id;
-//       let id2 = selectedTiles[1].id;
-//
-//       //map over the tile list and change the property of the two tiles to matched
-//       let xs = _.map(this.state.tiles, (tile) => {
-//         if(tile.id == id1 || tile.id == id2){
-//           return _.extend(tile, {matched: true});
-//         } else {
-//           return tile;
-//         }
-//       });
-//
-//       //add 2 to the number of tiles matched till now
-//       let matched = this.state.tilesMatched;
-//       matched = matched + 2;
-//
-//       //increases score according to constant
-//       newScore = newScore + addScore;
-//
-//       //updating the state
-//       let st = _.extend(this.state, {
-//         tiles: xs,
-//         tilesMatched: matched,
-//         score: newScore,
-//         paused: false
-//       });
-//
-//       this.setState(st);
-//
-//
-//     } else {
-//
-//       //reduce score according to constant
-//       newScore = newScore - reduceScore;
-//
-//       //flipping cards again as match not correct
-//      for (var i=0; i<selectedTiles.length; i++){
-//           this.flipCard(selectedTiles[i]);
-//         }
-//
-//         //updating state
-//         let st = _.extend(this.state, {
-//           score: newScore,
-//           paused: false
-//         });
-//
-//         this.setState(st);
-//
-//     }
-//
-//     //updating the state
-//     let newSelected = [];
-//     let st1 = _.extend(this.state, {
-//       selectedTiles: newSelected
-//     })
-//
-//   }
-//
--- hw05/memory/lib/memory/game.ex
+++ hw05/memory/lib/memory/game.ex
@@ -3,7 +3,7 @@ defmodule Memory.Game do
 
   def new do
     %{
-      tiles: createTileList(getShuffledList(), [], 0),
+      tiles: newTiles(),
       guessedTiles: [],
       tilesMatched: 0,
       selectedTiles: MapSet.new,
@@ -39,77 +39,52 @@ defmodule Memory.Game do
       else
         Map.replace!(tl,:val,"?")
       end
-  end
-end
-
-
-
-def clickTile(game, id) do
-  tls = game.tiles
-  selectTl = game.selectedTiles
-  paused = game.paused
-  # cond do
-  #   MapSet.size(selectTl) < 2 ->
-  #     #do something
-  #
-  # end
-  cond do
-    paused == true or
-    MapSet.size(selectTl) >= 3 or
-    isSelected(selectTl, id)->
-      game
-    MapSet.size(selectTl) < 3 ->
-      tls = Enum.map tls, fn tl ->
-              if Map.get(tl, :id) == id do
-                %{tl | flipped: true}
-              else
-                tl
-              end
-            end
-      tilesSelected = Enum.filter(tls, fn(t) ->
-        Map.get(t, :id) == id end)
-      selectTl = MapSet.put(selectTl, tilesSelected)
-      if MapSet.size(selectTl) == 2 do
-        game = Map.put(game, :tiles, tls)
-                |> Map.put(:selectedTiles, selectTl)
-                |> Map.put(:paused, true)
-        # flipBack(game)
-
-        # tick = spawn(Memory.Game, :handleTick, [game])
-        # send(tick, :flip)
-        # Process.send_after(self(), :flip, 1000)
-      else
-        game = Map.put(game, :tiles, tls)
-                |> Map.put(:selectedTiles, selectTl)
-      end
     end
-  game
-end
+  end
 
 
 
-def isSelected(selectTl, id) do
-  Enum.any? selectTl, fn t ->
-            Map.get(hd(t), :id) == id
-          end
-end
-#
-# def handleTick(game) do
-#   receive do
-#     :flip ->
-#       tls = game.tiles
-#       tls = Enum.map tls, fn tl ->
-#         if Map.get(tl, :matched) == false do
-#           %{tl | flipped: false}
-#         else
-#           tl
-#         end
-#       end
-#       Map.put(game, :tiles, tls)
-#        |> Map.put(:selectedTiles, MapSet.new)
-#        |> Map.put(:paused, false)
-#   end
-# end
+  def clickTile(game, id) do
+    tls = game.tiles
+    selectTl = game.selectedTiles
+    paused = game.paused
+    cond do
+      paused == true or
+      MapSet.size(selectTl) >= 3 or
+      isSelected(selectTl, id)->
+        game
+      MapSet.size(selectTl) < 3 ->
+        tls = Enum.map tls, fn tl ->
+                if Map.get(tl, :id) == id do
+                  %{tl | flipped: true}
+                else
+                  tl
+                end
+              end
+        tilesSelected = Enum.filter(tls, fn(t) ->
+          Map.get(t, :id) == id end)
+        selectTl = MapSet.put(selectTl, tilesSelected)
+        if MapSet.size(selectTl) == 2 do
+          # game =
+            Map.put(game, :tiles, tls)
+                  |> Map.put(:selectedTiles, selectTl)
+                  |> Map.put(:paused, true)
+        else
+          # game =
+            Map.put(game, :tiles, tls)
+                  |> Map.put(:selectedTiles, selectTl)
+        end
+      end
+    # game
+  end
+
+
+
+  def isSelected(selectTl, id) do
+    Enum.any? selectTl, fn t ->
+              Map.get(hd(t), :id) == id
+            end
+  end
 
   def checkMatch(game) do
     add_score = 20
@@ -121,33 +96,28 @@ end
       sltVals = Enum.map slt, fn t ->
                   Map.get(hd(t), :val) end
       tls = game.tiles
+
       if List.first(sltVals) == List.last(sltVals) do
-        # IO.inspect("IN LOOP")
-        # IO.inspect(List.first(sltVals))
-        # IO.inspect(List.last(sltVals))
-        tls = Enum.map tls, fn tl ->
-          if Map.get(tl, :val) == List.first(sltVals) do
-            %{tl | matched: true}
-          else
-            tl
+          tls = Enum.map tls, fn tl ->
+            if Map.get(tl, :val) == List.first(sltVals) do
+              %{tl | matched: true}
+            else
+              tl
+            end
           end
-        end
-        :timer.sleep(501)
-        sc = sc + add_score
-        tm = tm + 2
-        game = Map.put(game, :tiles, tls)
-        |> Map.put(:selectedTiles, MapSet.new)
-        |> Map.put(:paused, false)
-        |> Map.put(:score, sc)
-        |> Map.put(:tilesMatched, tm)
+          :timer.sleep(500)
+          sc = sc + add_score
+          tm = tm + 2
+          Map.put(game, :tiles, tls)
+                  |> Map.put(:selectedTiles, MapSet.new)
+                  |> Map.put(:paused, false)
+                  |> Map.put(:score, sc)
+                  |> Map.put(:tilesMatched, tm)
       else
-        :timer.sleep(1000)
-        game = flipBack(game)
+          :timer.sleep(1000)
+          flipBack(game)
       end
-
     end
-
-    game
   end
 
   def flipBack(game) do
@@ -169,38 +139,31 @@ end
   end
 
   def reset() do
-    new
+    new()
   end
 
   def newTiles() do
-    # rand
-    # temp
-
-    # shuffles the list
-    # for (var i=alphabets.length-1; i>=0; i--){
-    #   rand = Math.floor(Math.random() * alphabets.length);
-    #   temp = alphabets[rand];
-    #   alphabets[rand] = alphabets[i];
-    #   alphabets[i] = temp;
-    # }
-
     arr = getShuffledList()
+    # idCounter = 0
+    # Enum.map arr, fn it ->
+    #   %{:val => it, :flipped => false, :matched => false, :id => idCounter}
+    #   idCounter = idCounter + 1
+    # end
     createTileList(arr, [], 0)
-    # //adds Tiles to a new array
-    # for (var j = 0; j<alphabets.length; j++){
-    #   let newtile = { val: alphabets[j], flipped: false, matched: false, id: j};
-    #   newtiles.push(newtile);
-    # }
-
   end
 
   def getShuffledList() do
     alphabets = ["A","A","B","B","C","C","D","D","E","E","F","F","G","G","H","H"]
+    # idCounter = 0
     Enum.shuffle(alphabets)
+     # |> Enum.map fn it ->
+     #   %{:val => it, :flipped => false, :matched => false, :id => idCounter}
+     #   idCounter = idCounter + 1
+     # end
   end
 
-  def createTileList([], newTiles, counter) do
-    newTiles
+  def createTileList([], newtiles, counter) do
+    newtiles
   end
 
   def createTileList([head | tail], newtiles, counter) do
@@ -208,14 +171,4 @@ end
     counter = counter + 1
     createTileList(tail, newtiles, counter)
   end
-
-  def getScore() do
-    1
-  end
-
-  def getMatched() do
-    0
-  end
-
-
 end
